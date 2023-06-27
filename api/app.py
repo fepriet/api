631357e70d432c97034e7d04566981c0327d3e25
fepriet <101409739+fepriet@users.noreply.github.com>
2023-06-26 22:09:02 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,21 +1,21 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 import joblib
 import sklearn
-#curl -d "{\"Medidas\":[[2, 1, 123, 456, 1, 1, 1, 1, 3, 2, 1.0, 0.0, 0.0, 0.0, 1, 5, 2, 3, 1, 5000, 10000, 2, 4, 6]]}" -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/predecir
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'La pagina esta funcionando bien'
+    return app.send_static_file('index.html')
 
-@app.route("/predecir", methods=["POST"])
-def predecir():
-    json=request.get_json(force=True)
-    medidas=json['Medidas']
-    rf=joblib.load('modelo_entrenado.pkl')
-    prediccion=rf.predict(medidas)
-    return'Las medidas que diste corresponden a la clase {0}\n\n'.format(prediccion)
+@app.route("/api")
+def api():
+    medidas = [[2, 1, 123, 456, 1, 1, 1, 1, 3, 2, 1.0, 0.0, 0.0, 0.0, 1, 5, 2, 3, 1, 5000, 10000, 2, 4, 6]]
+    rf = joblib.load('modelo_entrenado.pkl')
+    prediccion = rf.predict(medidas)
+    return jsonify(prediccion.tolist())
 
 if __name__ == '__main__':
-    app.run
+    app.run()
+
 
